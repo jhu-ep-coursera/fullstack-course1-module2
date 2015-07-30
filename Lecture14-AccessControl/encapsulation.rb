797870b6dfb9593a75df49171d2577d5dfeb6ee5
Kalman Hazins <kalman.hazins@ghs.com>
2015-07-30 13:00:56 -0400

    private access

--- Lecture14-AccessControl/encapsulation.rb
+++ Lecture14-AccessControl/encapsulation.rb
@@ -2,10 +2,14 @@ class Car
   def initialize(speed, comfort)
     @rating = speed * comfort
   end
-
+   
+  # Can't SET rating from outside
   def rating
     @rating
   end
 end
 
-puts Car.new(4, 5).rating
+puts Car.new(4, 5).rating # => 20
+
+
+
