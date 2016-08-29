f63b46624eb674d9016bd6c253e14cccd31cac82
Constantine Lazarakis <klaz39054@gmail.com>
2016-08-29 21:00:16 +0300

    Learning from others
    Copied code from solution; for purpose of analysis/understanding through example

--- Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,14 +1,17 @@
 class Person
   #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
+  attr_accessor :first_name, :last_name
 
   #have a class attribute called `people` that holds an array of objects
   @@people = []
 
   #have an `initialize` method to initialize each instance
-  def initialize(x,y)#should take 2 parameters for first_name and last_name
+  def initialize(first_name,last_name)  #should take 2 parameters for first_name and last_name
     #assign those parameters to instance variables
+    self.first_name = first_name
+    self.last_name = last_name
     #add the created instance (self) to people class variable
+    @@people << self
   end
 
   #have a `search` method to locate all people with a matching `last_name`
@@ -16,11 +19,13 @@ class Person
     #accept a `last_name` parameter
     #search the `people` class attribute for instances with the same `last_name`
     #return a collection of matching instances
+    @@people.select {|person| person.last_name == last_name}
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
     #return a formatted string as `first_name(space)last_name`
+    "#{first_name} #{last_name}"
   end
 end
 
